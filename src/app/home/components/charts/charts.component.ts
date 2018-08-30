bda4f12971258baaf14996ac2a970264f05e5186
ksiabani <kostas.siabanis@gmail.com>
2018-08-30 17:19:41 +0300

    Add chart settings

--- src/app/home/components/charts/charts.component.ts
+++ src/app/home/components/charts/charts.component.ts
@@ -1,16 +1,16 @@
-import { Component } from "@angular/core";
-import { Select } from "@ngxs/store";
-import { SharedState } from "../../../shared/store/shared.state";
-import { RepoInfo } from "../../../shared/model/info.interface";
-import { Observable } from "rxjs";
-import { HomeState } from "../../store/home.state";
-import { Languages } from "../../../shared/model/languages.interface";
-import { ChartData, LineChartData } from "../../../shared/model/charts.interface";
+import { Component } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { SharedState } from '../../../shared/store/shared.state';
+import { RepoInfo } from '../../../shared/model/info.interface';
+import { Observable } from 'rxjs';
+import { HomeState } from '../../store/home.state';
+import { Languages } from '../../../shared/model/languages.interface';
+import { ChartData, LineChartData } from '../../../shared/model/charts.interface';
 
 @Component({
-  selector: "app-charts",
-  templateUrl: "./charts.component.html",
-  styleUrls: ["./charts.component.scss"]
+  selector: 'app-charts',
+  templateUrl: './charts.component.html',
+  styleUrls: ['./charts.component.scss']
 })
 export class ChartsComponent {
   @Select(SharedState.info)
@@ -22,5 +22,13 @@ export class ChartsComponent {
   @Select(HomeState.participation)
   participation$: Observable<LineChartData>;
 
-  constructor() {}
+  // charts options
+  xAxisLabel: string = 'Week';
+  yAxisLabel: string = 'Commits';
+  colorScheme: { domain: string[] } = {
+    domain: ['#FE7675', '#2095F2', '#4DAE4E', '#FE9900']
+  };
+
+  constructor() {
+  }
 }
